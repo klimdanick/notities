de14ac094212c728dbfb7b02bb1ce642ca3a6a70
klimdanick <klimdanick@gmail.com>
2024-10-05 00:09:47 +0200

    test

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express'
 import notesRoutes from './routes/notesRoutes'
-import { addUserToNote, createUser } from './database'
+import { NotesFromUser, UserHasNoteAccess, addUserToNote, createUser } from './database'
 
 const app = express()
 const port = 8081
@@ -14,3 +14,9 @@ app.use('/api', notesRoutes)
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
 })
+
+createUser("klimdanick", "pass")
+addUserToNote("klimdanick", "testNote")
+console.log(UserHasNoteAccess("klimdanick", "testNote"))
+console.log(UserHasNoteAccess("klimdanick", "testNote2"))
+console.log(NotesFromUser("klimdanick"))

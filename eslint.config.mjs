c72d7d56f7152ff24518ab202b1cdee92021b01e
tijmenspeek <tijmen@speekenbrink.com>
2024-10-04 23:05:17 +0200

    more ignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,4 +6,14 @@ import tseslint from 'typescript-eslint';
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    ignores: ['node_modules', 'dist', 'build', '*.js'],
+    rules: {
+      // ESLint rules
+      'no-unused-vars': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+      ],
+    },
+  }
 );

ba34095a5518d7f80ccde133d090ade9a436ea4c
klimdanick <klimdanick@gmail.com>
2024-10-04 23:52:36 +0200

    added addUserToNote function

--- src/database.ts
+++ src/database.ts
@@ -2,11 +2,20 @@ import sqlite3 from 'sqlite3'
 
 export const createUser = (username: String, password: String) => {
     let db = new sqlite3.Database('notes.db', sqlite3.OPEN_READWRITE, (err) => {
-        if (err) console.error(err.message);
-        console.log('Connected to the database.');
+        if (err) console.error(err.message)
     })
     db.serialize(() => {
         let querry = db.prepare("INSERT INTO Users (username, password, salt) VALUES (?, ?, ?);")
-        querry.run(username, password, 'salt');
+        querry.run(username, password, 'salt')
+    })
+}
+
+export const addUserToNote = (username: String, uuid: String) => {
+    let db = new sqlite3.Database('notes.db', sqlite3.OPEN_READWRITE, (err) => {
+        if (err) console.error(err.message)
+    })
+    db.serialize(() => {
+        let querry = db.prepare("INSERT INTO UserNote (username, uuid) VALUES (?, ?);")
+        querry.run(username, uuid)
     })
 }

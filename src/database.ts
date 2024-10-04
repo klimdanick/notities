0a4f02c45b48512fb2b156081a3b01e22a7e3f22
klimdanick <klimdanick@gmail.com>
2024-10-04 23:38:14 +0200

    db test

--- src/database.ts
+++ src/database.ts
@@ -1,7 +1,7 @@
 import sqlite3 from 'sqlite3'
 
 export const createUser = (username: String, password: String) => {
-    let db = new sqlite3.Database(':main:')
+    let db = new sqlite3.Database('notes.db')
     db.serialize(() => {
         let querry = db.prepare("INSERT INTO user (username, password, salt) VALUES (?, ?, ?);")
         querry.run(username, password, 'salt');

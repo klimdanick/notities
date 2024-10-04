0cbf34b22717853cfa0a86da0dff86f3b7f9d46e
klimdanick <klimdanick@gmail.com>
2024-10-04 23:46:19 +0200

    database shit

--- src/database.ts
+++ src/database.ts
@@ -1,7 +1,10 @@
 import sqlite3 from 'sqlite3'
 
 export const createUser = (username: String, password: String) => {
-    let db = new sqlite3.Database('notes.db', sqlite3.OPEN_READWRITE)
+    let db = new sqlite3.Database('notes.db', sqlite3.OPEN_READWRITE, (err) => {
+        if (err) console.error(err.message);
+        console.log('Connected to the database.');
+    })
     console
     db.serialize(() => {
         let querry = db.prepare("INSERT INTO user (username, password, salt) VALUES (?, ?, ?);")

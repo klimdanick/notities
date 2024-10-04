b70262451b1e0a329cc91d5cef8f98a2fd5726b6
tijmenspeek <tijmen@speekenbrink.com>
2024-10-04 22:40:25 +0200

    post note

--- src/note_handlers.ts
+++ src/note_handlers.ts
@@ -1,11 +1,34 @@
 import { Request, Response } from 'express'
+import { v4 as uuidv4 } from 'uuid'
+import { Note } from './types'
+import path from 'path'
+import * as fs from 'fs'
 
 export const getNotes = (req: Request, res: Response) => {
     res.json({ message: 'All notes' })
 }
 
 export const createNote = (req: Request, res: Response) => {
-    res.json({ message: 'Note created' })
+    const id = uuidv4()
+    if (!req.body.title) {
+        res.status(400).send('Enter a title!')
+    }
+
+    const note: Note = {
+        id: id,
+        title: req.body.title,
+        content: req.body.content,
+        created_at: new Date().toISOString(),
+    }
+
+    const filePath = path.join(__dirname, '../', 'notes', `${id}.json`)
+
+    fs.writeFile(filePath, JSON.stringify(req.body.content, null, 2), (err) => {
+        if (err) {
+            return res.status(500).json({ error: 'Failed to write to file' })
+        }
+        res.status(200).json({ message: `Note created ${id}`, note: note })
+    })
 }
 
 export const updateNote = (req: Request, res: Response) => {

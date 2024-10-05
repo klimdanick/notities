f234a7dea9af7717d79472d089a63885e04917f7
tijmenspeek <tijmen@speekenbrink.com>
2024-10-05 19:53:06 +0200

    use callbacks for db

--- src/database.ts
+++ src/database.ts
@@ -1,55 +1,167 @@
 import sqlite3 from 'sqlite3'
 
-export const createUser = (username: String, password: String) => {
-    const db = new sqlite3.Database('notes.db', sqlite3.OPEN_READWRITE, (err) => {
-        if (err) console.error(err.message)
-    })
+type UserNote = {
+    username: string
+    UUID: string
+}
+export const createUser = (
+    username: string,
+    password: string,
+    callback: (err: Error | null) => void
+) => {
+    const db = new sqlite3.Database(
+        'notes.db',
+        sqlite3.OPEN_READWRITE,
+        (err) => {
+            if (err) {
+                console.error(err.message)
+                callback(err)
+                return
+            }
+        }
+    )
+
     db.serialize(() => {
-        let querry = db.prepare("INSERT INTO Users (username, password, salt) VALUES (?, ?, ?);")
-        querry.run(username, password, 'salt')
+        // Check if the user already exists
+        db.get(
+            'SELECT username FROM Users WHERE username = ?',
+            [username],
+            (err, row) => {
+                if (err) {
+                    console.error(err.message)
+                    callback(err)
+                    return
+                }
+                if (!row) {
+                    const query = db.prepare(
+                        'INSERT INTO Users (username, password, salt) VALUES (?, ?, ?);'
+                    )
+                    query.run(
+                        username,
+                        password,
+                        'salt',
+                        (err: Error | null) => {
+                            query.finalize()
+                            if (err) {
+                                console.error(err.message)
+                                callback(err)
+                            } else {
+                                callback(null)
+                            }
+                        }
+                    )
+                } else {
+                    console.log('User already exists.')
+                    callback(new Error('User already exists.'))
+                }
+            }
+        )
     })
+
+    // Move db.close() inside the callback of the query
     db.close()
 }
 
-export const addUserToNote = (username: String, uuid: String) => {
-    const db = new sqlite3.Database('notes.db', sqlite3.OPEN_READWRITE, (err) => {
-        if (err) console.error(err.message)
-    })
-    db.serialize(() => {
-        let querry = db.prepare("INSERT INTO UserNote (username, uuid) VALUES (?, ?);")
-        querry.run(username, uuid)
-    })
-    db.close()
-}
+export const addUserToNote = (
+    username: string,
+    uuid: string,
+    callback: (err: Error | null) => void
+) => {
+    const db = new sqlite3.Database(
+        'notes.db',
+        sqlite3.OPEN_READWRITE,
+        (err) => {
+            if (err) {
+                console.error(err.message)
+                callback(err)
+                return
+            }
+        }
+    )
 
-export const NotesFromUser = (username: string) => {
-    let notes: string[] = [];
-    const db = new sqlite3.Database('notes.db', sqlite3.OPEN_READWRITE, (err) => {
-        if (err) console.error(err.message)
-    })
     db.serialize(() => {
-        let querry = db.prepare('SELECT uuid FROM UserNote WHERE username = "?";')
-        querry.each(username, (err, row: any) => {
-            if (err) console.log(err)
-            notes.push(row.uuid)
+        const query = db.prepare(
+            'INSERT INTO UserNote (username, uuid) VALUES (?, ?);'
+        )
+        query.run(username, uuid, (err: Error | null) => {
+            query.finalize()
+            if (err) {
+                console.error(err.message)
+                callback(err)
+            } else {
+                callback(null)
+            }
         })
     })
+
     db.close()
-    return notes
 }
 
-export const UserHasNoteAccess = (username: string, uuid: string) => {
-    let access: boolean = false;
-    const db = new sqlite3.Database('notes.db', sqlite3.OPEN_READWRITE, (err) => {
-        if (err) console.error(err.message)
-    })
+export const NotesFromUser = (
+    username: string,
+    callback: (notes: string[] | null, err: Error | null) => void
+) => {
+    const notes: string[] = []
+    const db = new sqlite3.Database(
+        'notes.db',
+        sqlite3.OPEN_READWRITE,
+        (err) => {
+            if (err) {
+                console.error(err.message)
+                callback(null, err)
+                return
+            }
+        }
+    )
+
     db.serialize(() => {
-        let querry = db.prepare('SELECT uuid FROM UserNote WHERE username = "?";')
-        querry.each(username, (err, row: any) => {
-            if (err) console.log(err)
-            if (row.uuid == uuid) access = true
-        })
+        const query = db.prepare('SELECT * FROM UserNote WHERE username = ?;')
+        query.each(
+            username,
+            (err, row: UserNote) => {
+                if (err) {
+                    console.log(err)
+                    callback(null, err)
+                    return
+                }
+                notes.push(row.UUID)
+            },
+            () => {
+                query.finalize()
+                callback(notes, null)
+            }
+        )
     })
+
     db.close()
-    return access
+}
+
+export const UserHasNoteAccess = (
+    username: string,
+    uuid: string,
+    callback: (access: boolean) => void
+) => {
+    const db = new sqlite3.Database(
+        'notes.db',
+        sqlite3.OPEN_READWRITE,
+        (err) => {
+            if (err) {
+                console.error(err.message)
+                callback(false)
+                return
+            }
+        }
+    )
+
+    const query = 'SELECT * FROM UserNote WHERE username = ?;'
+    db.get(query, [username], (err, row: UserNote) => {
+        if (err) {
+            console.log(err)
+            callback(false)
+        } else {
+            callback(row && row.UUID === uuid)
+        }
+
+        db.close()
+    })
 }
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,15 @@
 import express, { Request, Response } from 'express'
 import notesRoutes from './routes/notesRoutes'
-import { NotesFromUser, UserHasNoteAccess, addUserToNote, createUser } from './database'
 
 const app = express()
 const port = 8081
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript with Express!')
+    res.send('Hello, TypeScript with Express!')
 })
 
 app.use('/api', notesRoutes)
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
+    console.log(`Server running on port ${port}`)
 })
-
-createUser("klimdanick", "pass")
-addUserToNote("klimdanick", "testNote")
-console.log(UserHasNoteAccess("klimdanick", "testNote"))
-console.log(UserHasNoteAccess("klimdanick", "testNote2"))
-console.log(NotesFromUser("klimdanick"))

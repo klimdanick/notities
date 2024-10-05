14aef6d1be9bdeb288fa1a2804841a8f5f9c90b1
tijmenspeek <tijmen@speekenbrink.com>
2024-10-05 20:04:25 +0200

    add donfig

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,12 +7,12 @@ import eslintConfigPrettier from "eslint-config-prettier";
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  // @ts-ignore
   eslintConfigPrettier,
   {
     ignores: ['node_modules', 'dist', 'build', '*.js'],
     rules: {
       // ESLint rules
-      'no-unused-vars': 'warn',
       '@typescript-eslint/no-unused-vars': [
         'warn',
       ],

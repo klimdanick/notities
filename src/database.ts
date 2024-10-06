be2954c82ebe5001c6d013cb9f36899bdb76d704
klimdanick <klimdanick@gmail.com>
2024-10-06 17:21:24 +0200

    database shit gefixt

--- src/database.ts
+++ src/database.ts
@@ -1,167 +1,63 @@
-import sqlite3 from 'sqlite3'
+import sqlite from 'node:sqlite'
 
-type UserNote = {
-    username: string
-    UUID: string
-}
-export const createUser = (
-    username: string,
-    password: string,
-    callback: (err: Error | null) => void
-) => {
-    const db = new sqlite3.Database(
-        'notes.db',
-        sqlite3.OPEN_READWRITE,
-        (err) => {
-            if (err) {
-                console.error(err.message)
-                callback(err)
-                return
-            }
-        }
-    )
+export function createUser(username: string, password: string) {
+    const db = new sqlite.DatabaseSync("notes.db");
 
-    db.serialize(() => {
-        // Check if the user already exists
-        db.get(
-            'SELECT username FROM Users WHERE username = ?',
-            [username],
-            (err, row) => {
-                if (err) {
-                    console.error(err.message)
-                    callback(err)
-                    return
-                }
-                if (!row) {
-                    const query = db.prepare(
-                        'INSERT INTO Users (username, password, salt) VALUES (?, ?, ?);'
-                    )
-                    query.run(
-                        username,
-                        password,
-                        'salt',
-                        (err: Error | null) => {
-                            query.finalize()
-                            if (err) {
-                                console.error(err.message)
-                                callback(err)
-                            } else {
-                                callback(null)
-                            }
-                        }
-                    )
-                } else {
-                    console.log('User already exists.')
-                    callback(new Error('User already exists.'))
-                }
-            }
-        )
-    })
-
-    // Move db.close() inside the callback of the query
-    db.close()
-}
-
-export const addUserToNote = (
-    username: string,
-    uuid: string,
-    callback: (err: Error | null) => void
-) => {
-    const db = new sqlite3.Database(
-        'notes.db',
-        sqlite3.OPEN_READWRITE,
-        (err) => {
-            if (err) {
-                console.error(err.message)
-                callback(err)
-                return
-            }
-        }
-    )
-
-    db.serialize(() => {
-        const query = db.prepare(
-            'INSERT INTO UserNote (username, uuid) VALUES (?, ?);'
-        )
-        query.run(username, uuid, (err: Error | null) => {
-            query.finalize()
-            if (err) {
-                console.error(err.message)
-                callback(err)
-            } else {
-                callback(null)
-            }
-        })
-    })
+    try {
+        const query = db.prepare("INSERT INTO users (username, password, salt) VALUES (?, ?, ?)")
+        query.run(username, password, "salt")
+    } catch (err: any) {
+        return err
+    }
 
     db.close()
 }
 
-export const NotesFromUser = (
-    username: string,
-    callback: (notes: string[] | null, err: Error | null) => void
-) => {
-    const notes: string[] = []
-    const db = new sqlite3.Database(
-        'notes.db',
-        sqlite3.OPEN_READWRITE,
-        (err) => {
-            if (err) {
-                console.error(err.message)
-                callback(null, err)
-                return
-            }
-        }
-    )
+export function addUserToNode(username: string, uuid: string) {
+    if (hasNoteAccess(username, uuid)) return
+    const db = new sqlite.DatabaseSync("notes.db");
 
-    db.serialize(() => {
-        const query = db.prepare('SELECT * FROM UserNote WHERE username = ?;')
-        query.each(
-            username,
-            (err, row: UserNote) => {
-                if (err) {
-                    console.log(err)
-                    callback(null, err)
-                    return
-                }
-                notes.push(row.UUID)
-            },
-            () => {
-                query.finalize()
-                callback(notes, null)
-            }
-        )
-    })
+    try {
+        const query = db.prepare("INSERT INTO usernote (username, uuid) VALUES (?, ?)")
+        query.run(username, uuid)
+    } catch (err: any) {
+        return err
+    }
 
     db.close()
 }
 
-export const UserHasNoteAccess = (
-    username: string,
-    uuid: string,
-    callback: (access: boolean) => void
-) => {
-    const db = new sqlite3.Database(
-        'notes.db',
-        sqlite3.OPEN_READWRITE,
-        (err) => {
-            if (err) {
-                console.error(err.message)
-                callback(false)
-                return
-            }
-        }
-    )
+export function hasNoteAccess(username: string, uuid: string) {
+    const notes = notesFromUser(username)
+    return notes.includes(uuid)
+}
 
-    const query = 'SELECT * FROM UserNote WHERE username = ?;'
-    db.get(query, [username], (err, row: UserNote) => {
-        if (err) {
-            console.log(err)
-            callback(false)
-        } else {
-            callback(row && row.UUID === uuid)
-        }
+export function notesFromUser(username: string) {
+    const db = new sqlite.DatabaseSync("notes.db");
+    let results: any[] = []
+    try {
+        const query = db.prepare("SELECT UUID from usernote where username = ?")
+        results = query.all(username)
+        results = results.map(result => result.UUID)
+    } catch (err: any) {
+        console.log(err)
+    }
 
-        db.close()
-    })
+    db.close()
+    return results
+}
+
+export function usersFromNote(uuid: string) {
+    const db = new sqlite.DatabaseSync("notes.db");
+    let results: any[] = []
+    try {
+        const query = db.prepare("SELECT username from usernote where UUID = ?")
+        results = query.all(uuid)
+        results = results.map(result => result.UUID)
+    } catch (err: any) {
+        console.log(err)
+    }
+
+    db.close()
+    return results
 }

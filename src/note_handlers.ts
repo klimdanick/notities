396290ee7415ab25e7bf0815ebf00dfc7f299291
klimdanick <klimdanick@gmail.com>
2024-10-07 11:56:48 +0200

    delete note from db

--- src/note_handlers.ts
+++ src/note_handlers.ts
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from 'uuid'
 import { Note } from './types'
 import path from 'path'
 import * as fs from 'fs'
-import { addUserToNode, hasNoteAccess, notesFromUser } from './database'
+import { addUserToNode, deleteNoteFromDB, hasNoteAccess, notesFromUser } from './database'
 
 
 export const getNotes = (req: Request, res: Response) => {
@@ -14,14 +14,14 @@ export const getNotes = (req: Request, res: Response) => {
     const username = req.query.username as string
     const allowedNoteIds = notesFromUser(username)
     let notes = []
-    
+
     for (const id of allowedNoteIds) {
         const filePath = path.join(__dirname, '../', 'notes', `${id}.json`)
         const fileContent = fs.readFileSync(filePath)
         const note: Note = JSON.parse(fileContent.toString())
         notes.push(note)
     }
-    res.status(200).json(notes)    
+    res.status(200).json(notes)
 }
 
 
@@ -32,15 +32,15 @@ export const getNote = (req: Request, res: Response) => {
     }
     const id = req.query.id as string
     const username = req.query.username as string
-    if (!hasNoteAccess(username, id)){
+    if (!hasNoteAccess(username, id)) {
         res.status(403).json({ error: 'You do not have access to this note' })
         return
     }
     const filePath = path.join(__dirname, '../', 'notes', `${id}.json`)
     const fileContent = fs.readFileSync(filePath)
     const note: Note = JSON.parse(fileContent.toString())
-    res.status(200).json(note) 
-}   
+    res.status(200).json(note)
+}
 
 
 export const createNote = (req: Request, res: Response) => {
@@ -48,7 +48,7 @@ export const createNote = (req: Request, res: Response) => {
         res.status(400).send('Enter a title!')
         return
     }
-    if (!req.body.username){
+    if (!req.body.username) {
         res.status(400).send('Enter a username!')
         return
     }
@@ -67,7 +67,7 @@ export const createNote = (req: Request, res: Response) => {
     fs.writeFile(filePath, JSON.stringify(note, null, 2), (err) => {
         if (err) {
             return res.status(500).json({ error: 'Failed to write to file' })
-        }  
+        }
     })
     addUserToNode(username, id)
     res.status(200).json({ message: `Note created ${id}`, note: note })
@@ -109,7 +109,7 @@ export const deleteNote = (req: Request, res: Response) => {
     }
     const username = req.body.username
     const id = req.body.id
-    
+
     if (!hasNoteAccess(username, id)) {
         res.status(403).json({ error: 'You do not have access to this note' })
         return
@@ -117,5 +117,7 @@ export const deleteNote = (req: Request, res: Response) => {
 
     const filePath = path.join(__dirname, '../', 'notes', `${id}.json`)
     fs.unlinkSync(filePath)
+
+    deleteNoteFromDB(id)
     res.status(200).json({ message: `Note ${id} deleted` })
 }
